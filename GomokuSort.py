d4bdb6c9414c1e0870d4fc330ea0a768b9ff3edd
patchgi <pacchigi0910@gmail.com>
2015-07-06 12:02:27 +0900

    勝敗判定忘れてタンゴwwwwww

--- GomokuSort.py
+++ GomokuSort.py
@@ -28,6 +28,71 @@ def puttable(_x,_y):
 		return False
 	return True
 
+def judgeGame():
+	line=0
+	for i in xrange(SCALE):
+		if cells[0][i]!=0:
+			line=1
+			for j in xrange(SCALE):
+				if cells[0][i]!=cells[j][i]:
+					line=0
+					break
+		else:
+			line=0
+
+	if line!=0:
+		return cells[0][i]
+
+	for i in xrange(SCALE):
+		if cells[i][0]!=0:
+			line=1
+			for j in xrange(SCALE):
+				if cells[i][0]!=cells[j][i]:
+					line=0
+					break
+
+		else:
+			line=0
+
+	if line!=0:
+		return cells[i][0]
+
+	for i in xrange(1,SCALE):
+		if cells[0][0]!=0:
+			line=1
+			if cells[0][0]!=cells[i][i]:
+				line=0
+				break
+
+		else:
+			line=0
+
+	if line!=0:
+		return cells[0][0]
+
+	for i in xrange(1,SCALE):
+		if cells[0][SCALE-1]!=0:
+			line=1
+			if cells[0][SCALE-1]!=cells[i][SCALE-1-i]:
+				line=0
+				break
+
+		else:
+			line=0
+
+	if line!=0:
+		return cells[0][SCALE-1]
+
+
+	for i in xrange(SCALE):
+		for j in xrange(SCALE):
+			if cells[i][j]==0:
+				return 0
+
+	return 3
+
+
+
 def drawBoard():
 	raw=[" " for i in xrange(SCALE+1)]
 	raw[SCALE]=" ====="
@@ -45,7 +110,7 @@ def drawBoard():
 
 if __name__=="__main__":
 	drawBoard()
-	
+
 	while (True):
 		if turn:
 			while (True):
@@ -82,9 +147,16 @@ if __name__=="__main__":
 			put(eposX,eposY)
 		
 		drawBoard()
+
+		if judgeGame()==1:
+			print "1 WIN"
+			break
+		if judgeGame()==2:
+			print "2 WIN"
+			break
+		if judgeGame()==3:
+			print "DRAW"
+			break
+			
 		time.sleep(1)
 		turn =not turn
-
-		
-
-
